a97952a0f3b27f77284738ebe14fd7bd2c8eb220
Yamil <yamilg620@gmail.com>
2022-11-13 17:48:16 -0600

    Revisar proveedores

--- Sutidora_Gustazo/src/Modelo/DAOProveedores.java
+++ Sutidora_Gustazo/src/Modelo/DAOProveedores.java
@@ -3,77 +3,74 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Modelo;
+
 import java.sql.Date;
 import java.util.*;
+
 /**
  *
  * @author Usuario
  */
 public class DAOProveedores {
-    
+
     //Metodo para insertar datos a la base de datos
-    
-    public Proveedores Insertar(String ruc,String nomb, String numcelu, String direc ){
+    public Proveedores Insertar(String ruc, String nomb, String numcelu, String direc) {
         String transaccion = "INSERT INTO PROVEEDORES VALUES('"
-                +ruc+"', '"
-                +nomb+"', '"
-                +numcelu+"', '"
-                +direc+"')";
-                
-                
-        
+                + ruc + "', '"
+                + nomb + "', '"
+                + numcelu + "', '"
+                + direc + "')";
+
         //Llama al metodo actualizar ubicado en la Database.java
-        if (new DataBase(). Actualizar(transaccion)>0){
-            return new Proveedores (nomb, numcelu,direc);
+        if (new DataBase().Actualizar(transaccion) > 0) {
+            return new Proveedores(nomb, numcelu, direc);
         }
         return null;
-        
-        
+
     }
+
     //Metodo para actualizar un registro de la BD
-    public int Actualizar(String ruc, String nomb, String numcelu, String direc){
-        
+    public int Actualizar(String ruc, String nomb, String numcelu, String direc) {
+
         String transaccion = "UPDATE PROVEEDORES SET nomb='"
-                +nomb+ "',numcelu='"
-                +numcelu+"',direc='"
-                +direc+"' WHERE ruc="
-                +ruc;
-        
+                + nomb + "', numcelu='"
+                + numcelu + "', direc='"
+                + direc + "' WHERE ruc="
+                + ruc;
+
         return new DataBase().Actualizar(transaccion);
     }
+
     //Metodo para seleccionar todos los registros de la tabla
-    public List ObtenerDatos(){
-        
-        String transaccion ="SELECT * FROM PROVEEDORES";
+    public List ObtenerDatos() {
+
+        String transaccion = "SELECT * FROM PROVEEDORES";
         //Llama al metodo listar de la DataBase.java
-        
-        List<Map> registros = new  DataBase().Listar(transaccion);
+
+        List<Map> registros = new DataBase().Listar(transaccion);
         List<Proveedores> proveedores = new ArrayList();//Arreglo de proveedores
         //ciclo que recorre cada registro y los agrega al arreglo proveedores
-        for (Map registro : registros){
-            
-            Proveedores prov = new Proveedores ((String)registro.get("ruc"),
+        for (Map registro : registros) {
+
+            Proveedores prov = new Proveedores((String) registro.get("ruc"),
                     (String) registro.get("nomb"),
                     (String) registro.get("numcelu"),
                     (String) registro.get("direc"));
-                    
-                    
-                    
-                    proveedores.add(prov);
-            
+
+            proveedores.add(prov);
+
         }
-        
+
         return proveedores;//retorna todos los empelados ubicados  en la tabla de la BD
     }
+
     //Metodo para eliminar un registro de la tabla en la BD
-    public int Eliminar(String ruc){
-        
-        String transaccion = "DELETE FROM PROVEEDORES WHERE ruc='"+ruc+"'";
-        
+    public int Eliminar(String ruc) {
+
+        String transaccion = "DELETE FROM PROVEEDORES WHERE ruc='" + ruc + "'";
+
         return new DataBase().Actualizar(transaccion);
-        
+
     }
 
-    
-    
 }
